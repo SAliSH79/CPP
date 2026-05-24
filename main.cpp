9813cb4a70751244ec4ba1aa4ac272bb53246a48
SeyedAli SeyedHosseini <alihosseini797979@gmail.com>
2026-05-24 12:05:32 +0200

    CPP 1st try

---
+++ main.cpp
@@ -0,0 +1,13 @@
+// We need an IDE such as VS code
+// We need to install the C++ extension for VS code
+// We need to have a C++ compiler installed, such as g++
+
+#include <iostream>
+
+int main() {
+
+    auto result = (20 <=> 10) > 10;
+    std::cout << "Results: " << result << std::endl;
+    return 0;
+
+}
